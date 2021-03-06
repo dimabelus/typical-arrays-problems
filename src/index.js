2e4f397b83988da8a99ba63802fa62efb7fdab3a
dimabelus <dima21belus@gmail.com>
2021-03-07 00:35:26 +0300

    fix: implement correct function 'max' behavior

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 
 /*
-Using build-in function Math.min() in conjunction
+Using build - in function Math.min() in conjunction
+with Spread syntax(...) described in
+    Mozilla Development Network *
+*/
+exports.min = function min(array) {
+    //first check if it's undefined or null
+    if (typeof array === 'undefined' || array.length === 0) {
+        return 0;
+        //applying Math.min and Spread syntax
+    } else {
+        return Math.min(...array)
+    }
+}
+
+/*
+Using build-in function Math.max() in conjunction
 with Spread syntax (...) described in
 Mozilla Development Network
 */
-exports.min = function min (array) {
-  //first check if it's undefined or null
-   if (typeof array ==='undefined' || array.length === 0) {
-     return 0;
-  //applying Math.min and Spread syntax
-  } else {
-    return Math.min(...array)
-  }
+exports.max = function max(array) {
+    //first check if it's undefined or null
+    if (typeof array === 'undefined' || array.length === 0) {
+        return 0;
+        //applying Math.max and Spread syntax
+    } else {
+        return Math.max(...array)
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return 0;
 }
